3ac640e506cb3906d47b3749fab3345bc0334854
Gltched <78609482+Gltched@users.noreply.github.com>
2021-03-15 21:44:33 -0700

    Grass Block

--- main.js
+++ main.js
@@ -1,31 +1,57 @@
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
   70,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
-
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+window.addEventListener('resize', function() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix(update);
+});
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeMaterials =
+    [
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/grass_block/grass_block_side.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/grass_block/grass_block_side.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/grass_block/grass_block_top.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/dirt/dirt.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/grass_block/grass_block_side.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('textures/grass_block/grass_block_side.png'), side: THREE.DoubleSide})
+    ]
+
+var material = new THREE.MeshFaceMaterial(cubeMaterials);
+var grass_block = new THREE.Mesh(geometry, material);
+
+scene.add(grass_block);
 
 camera.position.z = 5;
 
-function animate() {
-  requestAnimationFrame(animate);
-  
-  cube.rotation.x += 0.01;
-  
-  renderer.render(scene, camera);
-}
+var update = function() {
+  //cube.rotation.y += 0.005;
+};
 
-animate();
+var render = function() {
+  renderer.render(scene, camera);
+};
+
+var GameLoop = function() {
+  requestAnimationFrame(GameLoop);
+  
+  update();
+  render();
+};
+
+GameLoop();
